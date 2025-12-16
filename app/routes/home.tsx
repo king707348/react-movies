221ef67047f03c4a954f8826bb0742f9ab4beaa7
Yu Cheng Hsu <vovojason@gmail.com>
2025-12-16 13:39:25 +0800

    載入shadcn 增加輪播 卡片

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Frontpage } from "../frontpage/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Frontpage />;
 }
